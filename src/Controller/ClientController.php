ea8c5bb38647cf9d5bbca10b99b948b78071ce8c
Dokoro <62444826+dokoro777@users.noreply.github.com>
2020-05-09 11:22:02 +0000

    update
    commit

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,14 +2,36 @@
 namespace App\Controller;
 
 
+use App\Entity\Client;
+use App\Repository\ClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class ClientController extends AbstractController {
 
-     public function index():Response{
+    private $em;
+    private $repository;
+    public function __construct(ClientRepository $repository, EntityManagerInterface $em)
+    {
+        //communik avk bd pour initialiser entitymana
+        $this->em=$em;
+        $this->repository=$repository;
+    }
 
-        return $this->render("page/client.html.twig");
+    public function index():Response{
+         $client= new Client();
+         $client->setAdresse("derkle")
+             ->setNom("Dokoro")
+             ->setPrenom("francky")
+             ->setCodeclient("1234");
+        $this->em->persist($client);
+        $this->em->flush();
+
+        return $this->render("page/client.html.twig",
+        [
+
+        ]);
     }
 
 }
